ecce28e5e557fe28aa6adf3dfc99d0f2583a6c3f
zhen7216 <1229383755@qq.com>
2018-01-24 20:12:18 +0800

    Initial Commit

---
+++ MessageForwarding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageForwarding
+//
+//  Created by zhen7216 on 2018/1/24.
+//  Copyright © 2018年 ChenZhen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
